9d6b8cf6a856bba00a04e78f7b7d9514def2e131
Zeppar <849166805@qq.com>
2017-05-16 22:00:17 +0800

    Add Singleton

---
+++ IOSProject/Assets/Scripts/Base/ZRMonoSingleton.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+namespace ZRFramework {
+	public abstract class ZRMonoSingleton<T> : MonoBehaviour where T : ZRMonoSingleton<T> {
+
+		protected static T instance = null;
+
+		public static T Instance() {
+			if (instance == null) {
+				instance = FindObjectOfType<T> ();
+				if (FindObjectsOfType<T> ().Length > 1) {
+					print ("More than 1 Object");
+					return instance;
+				}
+				if (instance == null) {
+					string instanceName = typeof(T).Name;
+					print ("Instance Name : " + instanceName);
+					GameObject instanceGo = GameObject.Find (instanceName);
+					if (instanceGo == null)
+						instanceGo = new GameObject (instanceName);
+					instance = instanceGo.AddComponent<T> ();
+					DontDestroyOnLoad (instanceGo);
+					print ("Add new singleton " + instance.name + " in Game!");
+				} else {
+					print ("Already exists : " + instance.name);
+				}
+			}
+			return instance;
+		}
+
+		protected virtual void OnDestory() {
+			instance = null;
+		}
+	}
+}
---
+++ IOSProject/Assets/Scripts/Base/ZRSingleton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+using System;
+
+namespace ZRFramework {
+	public abstract class ZRSingleton<T> where T : ZRSingleton<T> {
+
+		protected static T instance = null;
+
+		protected ZRSingleton() {
+		
+		}
+
+		public T Instance() {
+			if (instance == null) {
+				ConstructorInfo[] ctors = typeof(T).GetConstructors (BindingFlags.Instance | BindingFlags.NonPublic);
+				ConstructorInfo ctor = Array.Find (ctors, c => c.GetParameters ().Length == 0);
+				if (ctor == null)
+					throw new Exception ("Non-public ctor not Found!");
+				instance = ctor.Invoke (null) as T;
+			}
+			return instance;
+		}
+	}
+}
